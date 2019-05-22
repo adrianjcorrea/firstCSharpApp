08b500d6af51662760cbc412cad18e04b2ff136a
Jose Adrián Correa Méndez <jcorrea@TELEPROCORP.COM.MX>
2019-05-22 14:18:20 -0500

    Started toy Problems

---
+++ GreetYourself.cs
@@ -0,0 +1,19 @@
+﻿//Function is supposed to greet by saying hello.
+//printing yourName on a separate line.
+using System;
+
+namespace CalculatorApplication
+{
+    class GreetYourSelf
+    {
+        static void Main(string[] args)
+        {
+            string greet = "Hello";
+            string name = "Jose Adrian Correa mendez";
+            //This(space) defines a break for a new line.
+            string space = "\n";
+            Console.WriteLine("{0}{1}{2}", greet, space, name);
+            Console.ReadLine();
+        }
+    }
+}
